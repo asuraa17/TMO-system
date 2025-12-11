bb955d8f0b8693ccb8d237d0a22e43c21c6921f1
Anan Gurung <grgana25@gmail.com>
2025-12-11 17:28:22 +0545

    modification of registration pages, middleware paths and views:session data

--- users/middleware.py
+++ users/middleware.py
@@ -12,17 +12,17 @@ class AuthenticationMiddleware:
     def __call__(self, request):
         # paths that authenticated users should access
         restricted_paths = [
-            '/users/buyer/register/',
-            '/users/buyer/login/',
+            reverse("users:buyer_register"),
+            reverse("users:buyer_login"),
         ]
 
         if request.user.is_authenticated and request.path in restricted_paths:
 
             if hasattr(request.user, 'role') and request.user.role == 'buyer':
-                    return redirect('/users/buyer/home/')
+                    return redirect(reverse("users:buyer_home"))
                 
             elif hasattr(request.user, 'role'):
-                    return redirect('/users/dashboard/')
+                    return redirect(reverse("users:dashboard"))
 
         response = self.get_response(request)
         return response
